390de50308633ddb0e018a8bd22ed5199ab655d8
mdkn <m.kan0842024@gmail.com>
2026-02-14 10:17:59 +0900

    refactor: convert function declarations to arrow functions

--- flexbox/flex-layout-table/src/components/Footer.tsx
+++ flexbox/flex-layout-table/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 // Footer component with SCSS Modules
 import styles from './Footer.module.scss'
 
-export default function Footer() {
+const Footer = () => {
   return <footer className={styles.footer}>Flexbox Layout Demo</footer>
 }
+
+export default Footer
